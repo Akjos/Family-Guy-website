707116059dadafe1c7f751aca49a20acf842edec
Akjos <Sojka_p@o2.pl>
2017-03-03 12:11:31 +0100

    Checking form fields.

--- js/code.js
+++ js/code.js
@@ -10,26 +10,35 @@ window.onscroll = function () {
     fixedMenu();
 };
 
-
+function check(name, reg) {
+    if (!reg.test(name.value)) {
+        name.classList.remove("correctInput");
+        name.classList.add("incorrectInput");
+    } else {
+        name.classList.remove("incorrectInput");
+        name.classList.add("correctInput");
+        
+    }
+}
+function checkNameSurname() {
+    var name = this;
+    var reg = /^[A-Z]{1}[a-z]{2,}$/g;
+    check(name, reg);
+}
+function checkEmail() {
+    email = this;
+    var reg = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
+    check(email, reg);
+}
+function checkPhone() {
+    var phone = this;
+    var reg = /[0-9\s]{9}/;
+    check(phone, reg);
+}
 function checkForm() {
     document.querySelector('input[name="name"]').addEventListener('change', checkNameSurname);
     document.querySelector('input[name="surname"]').addEventListener('change', checkNameSurname);
     document.querySelector('input[name="e-mail"]').addEventListener('change', checkEmail);
-}
-function checkNameSurname() {
-    var nameVal = this.value;
-    console.log(nameVal);
-    var reg = /^[A-Z]{1}[a-z]{2,}$/g;
-    if (!reg.test(nameVal)) {
-        alert("Co to za dziwne imię?...");
-        this.select();
-    }
-}
-function checkEmail() {
-    emailVal = this.value;
-    var reg = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
-    if (!reg.test(emailVal)) {
-        console.log("no chyba nie")
-    }
+    document.querySelector('input[name="phone"]').addEventListener('change', checkPhone);
 }
 document.addEventListener("DOMContentLoaded", checkForm);
